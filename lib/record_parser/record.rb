61b3c4540733b8149a040f12d35e6b775d7286da
Michael H Buselli <cosine@cosine.org>
2018-09-12 09:42:19 -0500

    Record: input methods speced and implemented

--- lib/record_parser/record.rb
+++ lib/record_parser/record.rb
@@ -1,13 +1,17 @@
+require "date"
+
 class RecordParser
   class Record
-    attr_reader :last_name, :first_name, :gender, :favorite_color, :birth_date
+    FIELDS = [:last_name, :first_name, :gender, :favorite_color, :birth_date]
+    attr_reader *FIELDS
 
-    def initialize(last_name:, first_name:, gender:, favorite_color:, birth_date:)
-      @last_name = last_name
-      @first_name = first_name
-      @gender = parse_gender(gender)
-      @favorite_color = favorite_color
-      @birth_date = birth_date
+    def initialize(field_data)
+      raise InvalidFieldData if field_data.size != FIELDS.size
+      @last_name = field_data[0]
+      @first_name = field_data[1]
+      @gender = parse_gender(field_data[2])
+      @favorite_color = field_data[3]
+      @birth_date = parse_date(field_data[4])
     end
 
     SORT_ORDERS = {
@@ -22,10 +26,25 @@ class RecordParser
     private
 
     def parse_gender(gender)
-      case gender[0].downcase
+      case gender[0].to_s.downcase
         when "f" then :female
         when "m" then :male
         else :other
+      end
+    end
+
+    def parse_date(date)
+      # This is to catch case when we already are passed a Date or Date-like object.
+      if date.respond_to?(:to_date)
+        date.to_date
+
+      # Otherwise we assume a String was input, and we just need to figure out format.
+      elsif date =~ %r"\A\d+/\d+/\d+\z"
+        Date.strptime(date, "%m/%d/%Y")
+
+      # If the date string was not in the above format, Date.parse will do the right thing.
+      else
+        Date.parse(date)
       end
     end
   end
---
+++ spec/lib/record_parser/record_spec.rb
@@ -0,0 +1,40 @@
+require "spec_helper"
+
+describe RecordParser::Record do
+  it "assigns last name, first name, and favorite color correctly" do
+    record = RecordParser::Record.new(["Last", "First", "male", "green", "3/5/1999"])
+    expect(record.last_name).to eq("Last")
+    expect(record.first_name).to eq("First")
+    expect(record.favorite_color).to eq("green")
+  end
+
+  it "parses String inputs correctly with US dates" do
+    record = RecordParser::Record.new(["Last", "First", "male", "green", "3/5/1999"])
+    expect(record.birth_date).to eq(Date.new(1999, 3, 5))
+  end
+
+  it "parses String inputs correctly with dash format dates" do
+    record = RecordParser::Record.new(["Last", "First", "male", "green", "1997-06-15"])
+    expect(record.birth_date).to eq(Date.new(1997, 6, 15))
+  end
+
+  it "passes a Date object through correctly" do
+    record = RecordParser::Record.new(["Last", "First", "male", "green", Date.new(1994, 12, 31)])
+    expect(record.birth_date).to eq(Date.new(1994, 12, 31))
+  end
+
+  it "parses female gender correctly" do
+    record = RecordParser::Record.new(["Last", "First", "female", "green", "3/5/1999"])
+    expect(record.gender).to eq(:female)
+  end
+
+  it "parses male gender correctly" do
+    record = RecordParser::Record.new(["Last", "First", "male", "green", "3/5/1999"])
+    expect(record.gender).to eq(:male)
+  end
+
+  it "parses other gender correctly" do
+    record = RecordParser::Record.new(["Last", "First", "unknown", "green", "3/5/1999"])
+    expect(record.gender).to eq(:other)
+  end
+end
